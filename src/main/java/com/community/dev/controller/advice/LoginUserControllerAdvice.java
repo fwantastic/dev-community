3444d12671865d0d9a70f0cd2f6b3570e4d6c739
Ryan <jh3155@gmail.com>
2018-01-22 19:36:29 -0500

    making tags accessible everywhere

--- src/main/java/com/community/dev/controller/advice/LoginUserControllerAdvice.java
+++ src/main/java/com/community/dev/controller/advice/LoginUserControllerAdvice.java
@@ -1,18 +1,29 @@
 package com.community.dev.controller.advice;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.community.dev.persistence.Tag;
+import com.community.dev.service.TagService;
 import com.community.dev.util.LoginUtility;
 
 @ControllerAdvice
 public class LoginUserControllerAdvice {
 
+	@Autowired
+	private TagService tagService;
+
 	@ModelAttribute("loggedInUser")
 	public String getLoggedInUser() {
-
 		return LoginUtility.getLoggedInUserEmail();
+	}
 
+	@ModelAttribute("tags")
+	public List<Tag> getTags() {
+		return tagService.findAllByOrderByTagName();
 	}
 
 }
