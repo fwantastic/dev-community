303a35360645e3660af107eabe188b41ddb8796b
Ryan <jh3155@gmail.com>
2018-02-02 19:10:34 -0500

    updating home to redirect to articles

--- src/main/java/com/community/dev/controller/HomeController.java
+++ src/main/java/com/community/dev/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.community.dev.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@GetMapping("/")
-	public String index() {
-		return "/users";
+	public String index(Model model) {
+		return "redirect:/articles";
 	}
 
 }
