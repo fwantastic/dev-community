a0305ec93e387488dc412b24c4517f748d305f8d
Ryan <jh3155@gmail.com>
2018-02-12 21:48:43 -0500

    removing spaces

--- src/main/java/com/community/dev/util/LoginUtility.java
+++ src/main/java/com/community/dev/util/LoginUtility.java
@@ -7,15 +7,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class LoginUtility {
 
 	public static String getLoggedInUserEmail() {
-
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
 		if (auth != null && !(auth instanceof AnonymousAuthenticationToken)) {
 			return auth.getName();
 		}
-
 		return null;
-
 	}
 
 }
