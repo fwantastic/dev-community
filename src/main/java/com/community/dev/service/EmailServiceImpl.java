3abb746cd3b5cda90de5820a281db50b8c08c61c
Ryan <jh3155@gmail.com>
2018-11-05 21:53:39 -0500

    checking username before sending

--- src/main/java/com/community/dev/service/EmailServiceImpl.java
+++ src/main/java/com/community/dev/service/EmailServiceImpl.java
@@ -2,6 +2,9 @@ package com.community.dev.service;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -12,14 +15,25 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class EmailServiceImpl implements EmailService {
 
+	private static final Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
+
 	@Autowired
 	public JavaMailSender emailSender;
+
+	@Value("${spring.mail.username}")
+	private String emailUsername;
 
 	@Value("${spring.mail.sender}")
 	private String senderEmail;
 
 	@Override
 	public void sendSimpleMessage(String subject, String message) {
+
+		if (StringUtils.isBlank(emailUsername)) {
+			logger.warn("senderEmail is null. Email will not be sent.");
+			return;
+		}
+
 		SimpleMailMessage simpleMessage = new SimpleMailMessage();
 		simpleMessage.setTo(senderEmail);
 		simpleMessage.setSubject(subject);
